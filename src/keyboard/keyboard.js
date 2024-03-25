ff25da21ce49111c48d089e3b2705e5c8761a839
Walton Wang <yuwaltonwang@gmail.com>
2024-03-25 10:43:28 -0400

    auto message

---
+++ src/keyboard/keyboard.js
@@ -0,0 +1,40 @@
+const {fetchPostFn} = require("../utils/requests");
+const {GlobalKeyboardListener} = require("node-global-key-listener");
+const log = require("electron-log");
+
+const keyboardListener = (freezeDuration = 3 * 1000, uploadIntervalSeconds = 10 * 1000) => {
+    let clickTimestamps = [];
+    let server = null;
+
+    const addClick = () => {
+        const t = new Date();
+        if ((clickTimestamps.length > 0) && ((t - clickTimestamps[clickTimestamps.length - 1]) < freezeDuration)) {
+            return
+        }
+        clickTimestamps.push(t)
+    }
+
+    const uploadToServer = () => {
+        if (clickTimestamps.length === 0) return
+
+        log.info("Uploading keyboard activity...")
+        fetchPostFn(
+            "/keyboard/append",
+            {timestamps: clickTimestamps}
+        )
+        clickTimestamps = []
+    }
+    return {
+        start: () => {
+            setInterval(uploadToServer, uploadIntervalSeconds)
+            server = new GlobalKeyboardListener();
+            server.addListener(addClick);
+        },
+        stop: () => {
+            if (server === null) return
+            server.kill()
+        }
+    }
+}
+
+module.exports = {keyboardListener: keyboardListener()}
